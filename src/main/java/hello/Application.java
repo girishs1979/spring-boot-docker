93e86f1afce799cd90c9e1491c11cbf91dd42291
girish-s <girish-s@hcl.com>
2016-12-15 13:06:39 +0530

    ssssssssss

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 	public static void main(String[] args) {
         System.out.println("Hello");	
-//dddsssd
+/dddsssd
       SpringApplication.run(Application.class, args);
 	}
 
